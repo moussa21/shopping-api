24b0e5d4e04fef0aaa019fffc717b2b95ef149da
Moussa BALDE <mbalde@facilite.com>
2017-11-04 22:17:07 -0400

    create user entity

--- src/main/java/com/shopping/user/User.java
+++ src/main/java/com/shopping/user/User.java
@@ -1,10 +1,117 @@
 package com.shopping.user;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.validator.constraints.Email;
 
+@Entity
+@Table(name = "User")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
+	
+	private String name;
+	
+	private String firstName;
+	
+	private String phone;
+	
+	private Email email;
+
+	/**
+	 * 
+	 */
+	public User() {
+	}
+
+	/**
+	 * @param name
+	 * @param firstName
+	 * @param phone
+	 * @param email
+	 */
+	public User(String name, String firstName, String phone, Email email) {
+		this.name = name;
+		this.firstName = firstName;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public Long getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public Email getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param mail the email to set
+	 */
+	public void setEmail(Email email) {
+		this.email = email;
+	}
+	
+	
 }
